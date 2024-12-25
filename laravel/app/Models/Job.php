32ac705f024e4547e97b207afab793ecc5abfb36
augustoyuudi <augustoyuudi@gmail.com>
2024-12-24 23:19:53 -0300

    Lessons - Models - Migrations - $table and $fillable - php artisan tinker

--- laravel/app/Models/Job.php
+++ laravel/app/Models/Job.php
@@ -2,36 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Programmer',
-                'salary' => 10000
-            ],
-            [
-                'id' => 2,
-                'title' => 'Teacher',
-                'salary' => 40000
-            ],
-            [
-                'id' => 3,
-                'title' => 'Director',
-                'salary' => 200000
-            ]
-        ];
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
 
-    public static function find($id): array {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
-        
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
